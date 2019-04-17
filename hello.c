05c9e017005c01dd34f38f55f8f96977a84585f7
Kyle Theacos <c3328662@uon.edu.au>
2019-04-17 04:21:38 +0000

    Made it convert to capitals at the top. Simplified cased 1 and 2. case 3 not working tried hard coding substitution

--- hello.c
+++ hello.c
@@ -3,10 +3,17 @@
     int key= 1; // how far to rotate
     int count = 0; // initalised int count
     int choice; // intialised to use for switch statement
+    int k;
 int main()
 {
     printf("Enter the message:\n");
-    scanf("%[^\n]s",&str);
+    scanf("%[^\n]s",str);
+   for (count = 0; count < 100 && str[count] !='\0'; count++){
+   if(str[count] >= 97 && str[count] <= 122){
+			str[count] = str[count] - 32 ;
+    }
+   }
+   
     printf("Please select an option: \n"); // hardcoded a userinterface to give the user options
     printf("1) Encryption Rotation Cipher\n");
     printf("2) Decryption Rotation Cipher\n");
@@ -20,97 +27,63 @@ int main()
  { 
         case 1: 
         printf("You entered 1\n");
-    scanf("%[^\n]s",str);
+    
      for (count = 0; count < 100 && str[count] !='\0'; count++){
-      for(count = 0; str[count] != '\0'; count++){
-		if(str[count] >= 97 && str[count] <= 122){
-			str[count] = str[count]-32 + key;
-			
-			if(str[count] > 90){
-				str[count] = str[count] - 90 + 65 - 1;
-			}
-			
-		}
-		else if(str[count] >= 65 && str[count] <= 90){
+      if(str[count]>=65 && str[count]<=90){
 			str[count] = str[count] + key;
-			
-			if(str[count] > 90){
-				str[count] = str[count] - 90 + 65 - 1;
 			}
-		}
-	}
-	
+			if (str[count] > 90){
+			    str[count] = str[count] - 91 + 65;
+			}
+
     }
+
     printf("Encrypted message: %s\n", str);
 
      break;
-
     
         case 2: 
         printf("You entered 2\n");
-  scanf("%[^\n]s",str);
-    for (count = 0; count < 100 && str[count] !='\0'; count++){
   
-    for(count = 0; str[count] != '\0'; count++){
-		
-		
-		if(str[count] >= 97 && str[count] <= 122){
-			str[count] = str[count]-32 - key;
-			
-			if(str[count] > 90){
-				str[count] = str[count] - 90 + 65 - 1;
-			}
-			
-		}
-		else if(str[count] >= 65 && str[count] <= 90){
+     for (count = 0; count < 100 && str[count] !='\0'; count++){
+      {
 			str[count] = str[count] - key;
-			
-			if(str[count] > 90){
-				str[count] = str[count] - 90 + 65 - 1;
 			}
-		}
-	}
+			if (str[count] < 65){
+			    str[count] = str[count] + 91 - 65;
+			}
+			}
 	
-    }
+	
     printf("Decrypted message: %s\n", str);
 
     break;
 
 
         case 3: printf("You entered 3 \n");
+         
+     for (count = 0; count < 100 && str[count] !='\0'; count++){
+      {
+        switch (choice){
+            case 65: 'A';
+            str[count] = 'G';
+            case 90: 'Z';
+            str[count] = 'F';
+            
+        }
+
+
+	}
+	
+    }
+    printf("Encryption Substitution Cipher: %s\n", str);
          break;
         case 4: printf("You entered 4 \n");
+        printf("Decryption Substitution Cipher: %s\n", str);
          break;
         default: printf("You didn't select one of the preferences");
  }    
  return 0;
  }
- /*
-    printf("Enter the message to encrypt:\n");
-  scanf("%[^\n]s",str);
-    for (count = 0; count < 100 && str[count] !='\0'; count++){
-  
-    for(count = 0; str[count] != '\0'; count++){
-		
-		
-		if(str[count] >= 97 && str[count] <= 122){
-			str[count] = str[count]-32 + key;
-			
-			if(str[count] > 90){
-				str[count] = str[count] - 90 + 65 - 1;
-			}
-			
-		}
-		else if(str[count] >= 65 && str[count] <= 90){
-			str[count] = str[count] + key;
-			
-			if(str[count] > 90){
-				str[count] = str[count] - 90 + 65 - 1;
-			}
-		}
-	}
-	
-    }
-    printf("Encrypted message: %s\n", str);
-} */
+
 
